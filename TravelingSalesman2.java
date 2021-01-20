14c735bc805a0882cd1d257b5ddbfbe8e4894820
ryan-li20 <rli20@stuy.edu>
2021-01-20 00:00:17 -0500

    working i think

--- TravelingSalesman2.java
+++ TravelingSalesman2.java
@@ -28,6 +28,7 @@ public class TravelingSalesman2{
     // }
     ArrayList<ArrayList<String>> lines = new ArrayList<ArrayList<String>>();
     ArrayList<String> cities = new ArrayList<String>();
+    int bestest = 1000000;
     try{
       Scanner in = new Scanner(System.in);
       lines = scan(in, cities);
@@ -36,8 +37,33 @@ public class TravelingSalesman2{
       System.out.println("File not found");
   	  System.exit(1);
     }
-    System.out.println(cities.toString());
+    for(int i = 0; i < 2*foral(cities.size()); i++){
+      Collections.shuffle(cities);
+      int run = 0;
+      for(int j = 0; j < cities.size()-1; j++){
+        run += distance(cities.get(j), cities.get(j+1), lines);
+      }
+      if(run < bestest){
+        bestest = run;
+      }
+    }
+    System.out.println(bestest);
   }
+
+  public static int foral(int n){
+    int ret = 1;
+    if(n == 0){
+      return 0;
+    }
+    if(n == 1){
+      return 1;
+    }
+    for(int i = 1; i < n+1; i++){
+      ret = ret*i;
+    }
+    return ret;
+  }
+
   public static ArrayList<ArrayList<String>> scan(Scanner in, ArrayList<String> cty) throws FileNotFoundException{//add each city name and the distance to an arraylist, with the distance in position 0
     ArrayList<ArrayList<String>> lines = new ArrayList<ArrayList<String>>();
     int trips = 0;
