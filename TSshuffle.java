f3a1caa156d4e3d110cc674c459e7d785847879b
mzimic20 <>
2021-01-19 21:44:41 -0500

    matrix() copies values now

--- TSshuffle.java
+++ TSshuffle.java
@@ -16,6 +16,7 @@ public class TSshuffle {
       }
       System.out.println();
     }
+    System.out.println( findPath(N, data) );
   }
 
 // Creates a two-dimensional matrix to pull the length of paths between two cities later.
@@ -23,11 +24,11 @@ public class TSshuffle {
     int[][] arr = new int[n][n];
     int i = 0;
     int j = 0;
-    Scanner in = new Scanner("CitiesAndDistances.txt");
-    while( in.hasNext() ) {
+    Scanner in = new Scanner( System.in );
+    while( in.hasNext() && i < 8) {
       Scanner line = new Scanner( in.nextLine() );
       j = 0;
-      while( line.hasNext() ) {
+      while( line.hasNext() && j < 8) {
         // Adds 0 to the matrix if the path has already been covered or if it's mapping the city to itself.
         if (j <= i) {
           arr[i][j] = 0;
@@ -56,6 +57,39 @@ public class TSshuffle {
       return false;
     }
     return true;
+  }
+
+  public static int findPath(int n, int[][] data) {
+    int N = n;
+    int best = 1000000;
+    int t = 0;
+    for(int a = 0; a < N; a++) {
+      t = 0;
+      for(int b = 0; a < b && b < N; b++) {
+        t += data[a][b];
+        for(int c = 0; b < c && c < N; c++) {
+          t += data[b][c];
+          for(int d = 0; c < d && d < N; d++) {
+            t += data[c][d];
+            for(int e = 0; d < e && e < N; e++) {
+              t += data[d][e];
+              for(int f = 0; e < f && f < N; f++) {
+                t += data[e][f];
+                for(int g = 0; f < g && g < N; g++) {
+                  t += data[f][g];
+                  for(int h = 0; g < h && h < N; h++) {
+                    t += data[g][h];
+                    if (t < best) best = t;
+                    t = 0;
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+    return best;
   }
 
 }
